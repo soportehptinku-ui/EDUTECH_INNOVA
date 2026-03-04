25f58fdf3609f1d305f533457dbababf4a9aaf54
soportehptinku-ui <soporte.hptinku@gmail.com>
2026-03-04 14:45:42 -0500

    Delete edutech-innova directory

--- edutech-innova/src/lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-
-/**
- * Combina clases de Tailwind CSS de forma condicional.
- * Uso: cn('base-class', condition && 'conditional-class', { 'another': isActive })
- */
-export function cn(...inputs: ClassValue[]): string {
-  return clsx(inputs);
-}
